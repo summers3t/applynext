d1931bf03a366d8cdec5a35ce48b3ad2ade07aa5
summers3t <123099910+summers3t@users.noreply.github.com>
2025-07-09 00:10:35 +0300

    chore: clean backup after secret removal, all tests passing

--- .svelte-kit/types/src/routes/(app)/proxy+layout.server.ts
+++ .svelte-kit/types/src/routes/(app)/proxy+layout.server.ts
@@ -1,14 +1,8 @@
 // @ts-nocheck
-// src/routes/+layout.server.ts
 import type { LayoutServerLoad } from './$types';
 
 export const load = async ({ locals: { supabase } }: Parameters<LayoutServerLoad>[0]) => {
-  const {
-    data: { session },
-    error,
-  } = await supabase.auth.getSession();
-
+  const { data: { session }, error } = await supabase.auth.getSession();
   if (error) console.error('Session loading error:', error);
-
   return { session };
 };
